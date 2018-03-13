c1d51700bb83e87b53de92620582de91603d92f9
Admin Apple <adminapple@Ackers-MacBook-Pro-7.local>
2018-03-13 10:58:06 -0400

    x

--- 05_rgb_Acker2.js
+++ 05_rgb_Acker2.js
@@ -20,11 +20,6 @@ function ledColorSet(r_val, g_val, b_val){
 
 const op = new OrdinalProcessor()
 
-op.create("colorSetter", function(name, r_val, g_val, b_val){
-	console.log(name,r_val, g_val, b_val, "\n")
-	ledColorSet(r_val, g_val, b_val)
-})
-
 op.createFeed("colorFeed", function(sets){
 	const rHigh = sets.r_val===255
 	const gHigh = sets.g_val===255
@@ -61,56 +56,19 @@ op.createFeed("colorFeed", function(sets){
 	return sets
 })
 
-function main(){
-	if(wpi.wiringPiSetup() == -1){ //when initialize wiring failed, console.log messageto screen
-		console.log("setup wiringPi failed !");
-		return 1; 
-	}
-
-	ledInit();
-
-	console.log("\n");
-	console.log("\n");
-	console.log("========================================\n");
-	console.log("|              Breath LED              |\n");
-	console.log("|    ------------------------------    |\n");
-	console.log("|       Red Pin connect to GPIO0       |\n");
-	console.log("|      Green Pin connect to GPIO1      |\n");
-	console.log("|       Blue Pin connect to GPIO2      |\n");
-	console.log("|                                      |\n");
-	console.log("|  Make a RGB LED emits various color  |\n");
-	console.log("|                                      |\n");
-	console.log("|                            SunFounder|\n");
-	console.log("========================================\n");
-	console.log("\n");
-	console.log("\n");
-
-	op.colorFeed({
-		lighting : true,
-		r_val:0,
-		g_val:0,
-		b_val:0
-	})
-	.delay(10)
-	.rerun()
-
-/*
-	op
-	.colorSetter("Red",0xff,0x00,0x00)
-	.delay(500)
-	.colorSetter("Green", 0x00,0xff,0x00)//green
-	.delay(500)
-	.colorSetter("Blue", 0x00,0x00,0xff)//blue
-	.delay(500)
-	.colorSetter("Yellow", 0xff,0xff,0x00)//yellow
-	.delay(500)
-	.colorSetter("Purple", 0xff,0x00,0xff)//purple
-	.delay(500)
-	.colorSetter("Cyan", 0xc0,0xff,0x3e)//cyan
-	.delay(500)
-	.rerun()
-*/
-	return 0;
+if(wpi.wiringPiSetup() == -1){ //when initialize wiring failed, console.log messageto screen
+	console.log("setup wiringPi failed !");
+	return 1; 
 }
 
-main()
+ledInit();
+
+
+op.colorFeed({
+	lighting : true,
+	r_val:0,
+	g_val:0,
+	b_val:0
+})
+.delay(10)
+.rerun()
